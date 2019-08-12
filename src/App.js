5450427719f14f816fbe7f3f6e6d782e186b6fae
JasonYoo1 <jyoojs@gmail.com>
2019-08-12 11:39:34 -0700

    changed to context API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-import {ProductContext} from './contexts/ProductContext'
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import {ProductContext} from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
 
 function App() {
 	const [products] = useState(data);
@@ -18,17 +19,17 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<ProductContext.Provider value={{ products, addItem }}>
 			<Route exact path="/" component={Products} />
+			<CartContext.Provider value={{ cart }}>
+			<Navigation cart={cart} />
+			<Route exact path="/cart" component={cart} />
+			</CartContext.Provider>
 			</ProductContext.Provider>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+
 		</div>
 	);
 }
